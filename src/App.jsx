b2ba0f60d0bbf5ff5ec5a85689b6c94fcb1d0992
inehadev <156919946+inehadev@users.noreply.github.com>
2024-04-25 22:28:18 +0530

    created header

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from '@chakra-ui/react'
-
+import Header from './/assets/Components/Header'
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-     <div className='bg-red-500'> Hello</div>
-     <Button>hello</Button>
+    
+    <Header/>
+     
      
          </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,39 @@
-import * as React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
-import * as ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import {mode} from '@chakra-ui/theme-tools'
 
-const rootElement = document.getElementById('root')
-ReactDOM.createRoot(rootElement).render(
+
+const styles = {
+  global:(props)=>({
+    body: {
+      color:mode('gray.800' , 'whiteAlpha.900')(props),
+      bg:mode('gray.100' , '#101010')(props)
+    }
+  })
+};
+
+
+const config = {
+  initialColorMode:"dark",
+  useSystemColorMode:true
+}
+
+const color={
+  gray:{
+    dark:"#1e1e1e",
+    light:"#616161"
+  }
+}
+
+const theme = extendTheme ({config , styles, color})
+
+ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme ={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
